fea5ec0397b631867c098de488398f84677974b7
Nathaniel Schanno Johnson <schanno@stanford.edu>
2019-11-18 14:05:27 -0800

    fixed printing

--- google_stt.py
+++ google_stt.py
@@ -2,6 +2,7 @@ import requests
 import os
 import io
 import wave
+import pickle
 from google.cloud import speech_v1p1beta1 as speech
 from google.cloud.speech import enums
 from google.cloud.speech import types
@@ -46,7 +47,8 @@ def upload_blob(bucket_name, source_file_name, destination_blob_name):
 def transcribe_gcs(gcs_uri):
     """Asynchronously transcribes the audio file specified by the gcs_uri."""
 
-    credential_path = "G:\My Drive\AVP_data\polis_api\\avp_keys_svc.json"
+    ##Path to service account credentials
+    credential_path = "G:\My Drive\AVP_data\polis_api\\hs-american-voices-project-b12d8ea9a82f.json"
     os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = credential_path
     client = speech.SpeechClient()
 
@@ -64,31 +66,41 @@ def transcribe_gcs(gcs_uri):
     operation = client.long_running_recognize(config, audio)
 
     print('Waiting for operation to complete...')
-    response = operation.result(timeout=600)
+    response = operation.result(timeout=1800)
     wanted_result = response.results[-1]
+    with open('out_work.pkl', 'wb') as f:
+        pickle.dump(wanted_result, f)
 
     # Each result is for a consecutive portion of the audio. Iterate through
     # them to get the transcripts for the entire audio file.
+    output=[]
     prev_speaker_tag = wanted_result.alternatives[0].words[0].speaker_tag
     s = 'Speaker {}:'.format(prev_speaker_tag)
     for i in wanted_result.alternatives[0].words:
         if(i.speaker_tag != prev_speaker_tag):
-            print(s + '\n')
+            output.append(s+'\n')
             s = 'Speaker {}:'.format(i.speaker_tag)
         s += " "+i.word
         prev_speaker_tag = i.speaker_tag
-    print(s+'\n')
         # The first alternative is the most likely one for this portion.
+
+    output.append(s+'\n')
+    return(output)
 
 ## Upload test file to GCS
 
 ## Transcribe Test File
 ## Try with shorter file "Baltimore Short.mp3"
 
-filename_mp3 = "G:\My Drive\AVP_data\polis_api\\audio\Audio_202405 - Camilla Camargo.MP3"
-filename_wav = "G:\My Drive\AVP_data\polis_api\\audio\Audio_202405 - Camilla Camargo.wav"
+filename_mp3 = "G:\My Drive\AVP_data\polis_api\\audio\Baltimore 10min.MP3"
+filename_wav = "G:\My Drive\AVP_data\polis_api\\audio\Baltimore 10min.wav"
 
 ## Real File (convert to wave)
-mp3_to_wav(filename_mp3)
-stereo_to_mono(filename_wav)
-print(frame_rate_channel(filename_wav))
+# mp3_to_wav(filename_mp3)
+# stereo_to_mono(filename_wav)
+to_print = transcribe_gcs("gs://avp-audio/Baltimore 10min.wav")
+print(to_print[0])
+with open("transcript.txt", "w") as fout:
+    fout.writelines(to_print)
+    fout.close()
+
